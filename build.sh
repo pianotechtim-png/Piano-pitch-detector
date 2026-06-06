0b244c1223356cbd8585212303965ae9a3f6293f
pianotechtim-png <pianotechtim@gmail.com>
2026-06-05 23:13:39 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 pip install --upgrade pip
-pip install --only-binary=:all: numpy scipy==1.11.4
-pip install flask flask-cors librosa==0.10.1 soundfile audioread gunicorn
+pip install --only-binary=:all: numpy scipy
+pip install flask flask-cors librosa soundfile audioread gunicorn
